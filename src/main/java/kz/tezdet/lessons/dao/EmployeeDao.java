1a7793f5d3b24f3d6dc84eca93d8bd2aa69895ea
daulet <daucloudlab@gmail.com>
2016-05-12 20:51:17 +0600

    version 01

---
+++ src/main/java/kz/tezdet/lessons/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package kz.tezdet.lessons.dao;
+
+import kz.tezdet.lessons.models.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+    Employee findById(int id);
+
+    void saveEmployee(Employee employee);
+
+    void deleteEmployeeBySsn(String ssn);
+
+    List<Employee> findAllEmployees();
+
+    Employee findEmployeeBySsn(String ssn);
+
+}
